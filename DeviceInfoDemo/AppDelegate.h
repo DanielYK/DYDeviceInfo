c2e4f8c4c1c9676da4bb0f8d6eedf0730b6ab0a5
DanielYao <yao@whosv.com>
2017-03-31 12:38:41 +0800

    first

---
+++ DeviceInfoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeviceInfoDemo
+//
+//  Created by Daniel Yao on 17/3/23.
+//  Copyright © 2017年 Daniel Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeviceInfoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DeviceInfoDemo
+//
+//  Created by Daniel Yao on 17/3/23.
+//  Copyright © 2017年 Daniel Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
